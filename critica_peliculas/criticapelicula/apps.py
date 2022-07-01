c0ca17c5eef30faa28587525c887bf06e299bc89
MatiasPiantoni <106635283+MatiasPiantoni@users.noreply.github.com>
2022-07-01 18:45:31 -0300

    Delete critica_peliculas directory

--- critica_peliculas/criticapelicula/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class CriticapeliculaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'criticapelicula'
--- critica_peliculas/criticapelicula/views.py
+++
@@ -1,9 +0,0 @@
-from re import template
-from django.shortcuts import render
-from django.views import generic
-from django.views.generic import TemplateView
-
-from criticapelicula.models import Actor, Pelicula
-# Create your views here.
-class HomeView(TemplateView):
-    template_name = "home.html"
